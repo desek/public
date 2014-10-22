9c14077040f191df742ca776a84c9d3c295432b5
desek <daniel@grenemark.se>
2014-10-22 16:32:54 +0200

    ETL Processing

---
+++ Start-SCDWEtlProcessing.ps1
@@ -0,0 +1,193 @@
+﻿function Start-SCDWEtlProcessing
+{
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+    [CmdletBinding()]
+    Param(
+        [String]$ComputerName,
+        [PSCredential]$Credential,
+        [ValidateScript({$_ -ge 4})]
+        [int]$SleepTimer=120,
+        [String[]]$JobTypes,
+        [String]$DateFormat="HH:mm:ss",
+        # TODO WRITE PROGRESS
+        [switch]$WithProgress
+    )
+    BEGIN
+    {
+        # Verify Job Types
+        $ValidJobTypes = @(
+            "DWMaintenance",
+            "MPSyncJob",
+            "Extract",
+            "Transform",
+            "Load",
+            "Process"
+        )
+
+        Throw "Valid JobTypes are: $($JobTypes -join ", ")"
+
+        # Set Error Preference
+        If ($PSBoundParameters.ContainsKey("ErrorAction"))
+        {
+            $ErrorActionPreference = $ErrorAction
+        }
+        # Set Verbose and/or Debug
+        If ($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $PSDefaultParameterValues += @{"*:Verbose"=$true}
+        }
+        If ($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $PSDefaultParameterValues += @{"*:Debug"=$true}
+        }
+
+        # Set PSDefaultParameter for ComputerName and Credential
+        If ($PSBoundParameters.ContainsKey("ComputerName"))
+        {
+            $PSDefaultParameterValues += @{"*-SCDW*:ComputerName"=$ComputerName}
+        }
+        If ($PSBoundParameters.ContainsKey("Credential"))
+        {
+            $PSDefaultParameterValues += @{"*-SCDW*:Credential"=$Credential}
+        }
+
+        # Try to load SCSM DW module
+        Try
+        {
+            $smDir = (Get-ItemProperty "HKLM:\Software\Microsoft\System Center\2010\Service Manager\Setup").InstallDirectory
+            Import-Module "$smDir\Microsoft.EnterpriseManagement.Warehouse.Cmdlets.psd1"
+        }
+        Catch
+        {
+            Throw $_.Exception.Message
+        }
+    }
+
+    PROCESS
+    {
+        # Get all SCSM DW Jobs
+        Try
+        {
+            Write-Verbose "[$(Get-Date -Format $DateFormat)] Trying to get SCDW Jobs."
+            $Jobs = Get-SCDWJob
+        }
+        Catch
+        {
+            Throw $_.Exception.Message
+        }
+        Write-Verbose "[$(Get-Date -Format $DateFormat)] Successfully got $($Jobs.Count) SCDW Job(s)."
+
+        # Disable and Stop all SCSM DW Jobs
+        Write-Verbose "[$(Get-Date -Format $DateFormat)] Trying to Disable and Stop $($Jobs.Count) SCDW Job(s)."
+        $ErrorCount = 0        
+        Foreach ($Job in $Jobs)
+        {
+            Try
+            {
+                Disable-SCDWJobSchedule -JobName $Job.Name
+                Disable-SCDWJob -JobName $Job.Name
+                Stop-SCDWJob -JobName $Job.Name
+                Start-Sleep -Seconds ($SleepTimer/4)
+            }
+            Catch
+            {
+                Write-Error $_.Exception.Message
+                $ErrorCount++
+            }
+        }
+        
+        Write-Verbose "[$(Get-Date -Format $DateFormat)] Disabled and Stopped $($Jobs.Count) SCDW Job(s) with $($ErrorCount) Error(s)."
+        Remove-Variable ErrorCount
+
+        Write-Verbose "[$(Get-Date -Format $DateFormat)] Sleeping for $($SleepTimer*2) second(s)."
+        Start-Sleep -Seconds ($SleepTimer*2)
+
+        # Process all SCSM DW Jobs
+        $JobCurrent = 0
+        $JobCount = $Jobs.Count
+        $JobTypeCurrent = 0
+        $JobTypeCount = $JobTypes.Count
+        Foreach ($JobType in $JobTypes)
+        {
+            $JobTypeStart = Get-Date
+            $JobTypeCurrent++
+            $JobTypeJobCurrent = 0
+            $JobTypeJobCount = ($Jobs | Where-Object {$_.Name -like "$($JobType)*"}).Count
+            Write-Verbose "[$(Get-Date -Format $DateFormat)] $($JobType): Starting ($($JobTypeCurrent)/$($JobTypeCount))"
+            Foreach ($Job in ($Jobs | Where-Object {$_.Name -like "$($JobType)*"}))
+            {
+                $JobTypeJobStart = Get-Date
+                $JobTypeJobCurrent++
+                Write-Verbose "[$(Get-Date -Format $DateFormat)] $($Job.Name): Starting ($($JobTypeJobCurrent)/$($JobTypeJobCount))"
+                Enable-SCDWJob -JobName $Job.Name
+                Write-Verbose "[$(Get-Date -Format $DateFormat)] Sleeping for $SleepTimer second(s)."
+                Start-Sleep -Seconds $SleepTimer
+                Start-SCDWJob -JobName $Job.Name
+                do {
+                    Write-Verbose "[$(Get-Date -Format $DateFormat)] $($Job.Name): Processing ($($JobTypeJobCurrent)/$($JobTypeJobCount))"
+                    Start-Sleep -Seconds $SleepTimer
+                } while ((Get-SCDWJob -JobName $Job.Name).Status -eq "Running")
+                $JobTypeJobTimeSpan = New-TimeSpan -Start $JobTypeJobStart -End (Get-Date)
+                Write-Verbose "[$(Get-Date -Format $DateFormat)] $($Job.Name): Completed in $($JobTypeJobTimeSpan.ToString()) ($($JobTypeJobCurrent)/$($JobTypeJobCount))"
+            }
+            $JobTypeTimeSpan = New-TimeSpan -Start $JobTypeStart -End (Get-Date)
+            Write-Verbose "[$(Get-Date -Format $DateFormat)] $($JobType): Completed in $($JobTypeTimeSpan.ToString()) ($($JobTypeCurrent)/$($JobTypeCount))"
+        }
+
+        # Update SCSM DW Job Status
+        Try
+        {
+            $Jobs = Get-SCDWJob
+        }
+        Catch
+        {
+            Throw $_.Exception.Message
+        }
+
+        # Enable all SCSM DW Jobs
+        Write-Verbose "[$(Get-Date -Format $DateFormat)] Trying to Enable $($Jobs.Count) SCDW Job(s)."
+        $ErrorCount = 0
+        Foreach ($Job in $Jobs)
+        {
+            Try
+            {
+                If ($Job.IsEnabled -eq $false)
+                {
+                    Enable-SCDWJob -JobName $Job.Name
+                }
+                Enable-SCDWJobSchedule -JobName $Job.Name
+            }
+            Catch
+            {
+                Write-Error $_.Exception.Message
+            }
+        }
+
+        Write-Verbose "[$(Get-Date -Format $DateFormat)] Enabled $($Jobs.Count) SCDW Job(s) with $($ErrorCount) Error(s)."
+        Remove-Variable ErrorCount
+    }
+    END
+    {
+    }
+}
---
+++ msft2014.ps1
@@ -0,0 +1,87 @@
+﻿function Get-MSFT2014TechnicalPreview
+{
+    [CmdletBinding()]
+    Param(
+        [ValidateScript({Test-Path $_})]
+        [string]$Path=(Get-Location),
+        [Switch]$WithProgress
+    )
+    BEGIN
+    {
+        
+        $WebClient = New-Object System.Net.WebClient
+        $Uris = @{
+            rsatWin10x64 = "http://download.microsoft.com/download/2/7/9/27916E59-B7B5-4462-B95F-89DF4CE291D3/WindowsTH-KB2693643-x64.msu"
+            rsatWin10x86 = "http://download.microsoft.com/download/2/7/9/27916E59-B7B5-4462-B95F-89DF4CE291D3/WindowsTH-KB2693643-x86.msu"
+            WindowsServer = "http://care.dlservice.microsoft.com/dl/download/F/5/7/F574727C-B145-4A7D-B85B-11C4E8DC894B/9841.0.140912-1613.FBL_RELEASE_SERVER_OEM_X64FRE_EN-US.ISO"
+            SCDPM = "http://care.dlservice.microsoft.com/dl/download/evalx/sctp2014/SCTP_SCDPM.exe"
+            SCOM = "http://care.dlservice.microsoft.com/dl/download/evalx/sctp2014/SCTP_SCOM.exe"
+            SCVMM = "http://care.dlservice.microsoft.com/dl/download/evalx/sctp2014/SCTP_SCVMM.exe"
+            SCO = "http://care.dlservice.microsoft.com/dl/download/evalx/sctp2014/SCTP_SCO.exe"
+            SCSM = "http://care.dlservice.microsoft.com/dl/download/evalx/sctp2014/SCTP_SCSM.exe"
+        }
+
+        If ($WithProgress)
+        {
+            $DownloadSizeTotal = 0
+            $i = 0
+            Foreach ($Uri in $Uris.Keys)
+            {
+                $i++
+                Write-Progress -Id 1 -Activity 'Calculating Total Download Size' -Status "Item $i of $($Uris.Count) - $Uri" -PercentComplete (($i/$Uris.Count) * 100)
+
+                $WebRequest = [System.Net.WebRequest]::Create($Uris[$Uri])
+                $WebResponse = $WebRequest.GetResponse()
+                $DownloadSizeTotal += $WebResponse.ContentLength
+                $WebResponse.Close()
+                $WebRequest.Abort()
+            }
+            $DownloadSizeTotalMB = [System.Math]::Round(($DownloadSizeTotal/1024/1024),2)
+        }
+    }
+    PROCESS
+    {
+        $i = 0
+        $DownloadCurrentTotal = 0
+        $DownloadCurrentTotalMB = 0
+        Foreach ($Uri in $Uris.Keys)
+        {
+            $i++
+            Write-Host "$($Uri): Starting download"
+            $FileName = $Uris[$Uri].Split("/")[-1]
+            $OutFile = "$Path\$FileName"
+            
+            $WebRequest = [net.WebRequest]::Create($Uris[$Uri])
+            $WebResponse = $WebRequest.GetResponse()
+            $DownloadSize = $WebResponse.ContentLength
+            $WebResponse.Close()
+            $WebRequest.Abort()
+            $DownloadSizeMB = [System.Math]::Round(($DownloadSize/1024/1024),2)
+            
+            Try
+            {
+                $WebClient.DownloadFileAsync($Uris[$Uri],$OutFile)
+            }
+            Catch
+            {
+                Write-Host "$($Uri): Download failed" -ForegroundColor Red
+            }
+
+            While ((Get-Item $OutFile).Length -lt $DownloadSize) {
+                $DownloadCurrentSize = (Get-Item $OutFile).Length
+                $DownloadCurrentSizeMB = [System.Math]::Round(($DownloadCurrentSize/1024/1024),2)
+                $DownloadCurrentTotal = $DownloadedSize + $DownloadCurrentSize
+                $DownloadCurrentTotalMB = [System.Math]::Round(($DownloadCurrentTotal/1024/1024),2)
+                If ($WithProgress)
+                {
+                    Write-Progress -id 1 -Activity "Downloading Item $i of $($Uris.Count)" -Status "$DownloadCurrentTotalMB of $DownloadSizeTotalMB MB" -PercentComplete (($DownloadCurrentTotal/$DownloadSizeTotal) * 100)
+                    Write-Progress -id 2 -Activity "Downloading $Uri" -Status "$DownloadCurrentSizeMB of $DownloadSizeMB MB" -PercentComplete (((Get-Item $OutFile).Length / $DownloadSize)*100)
+                    Start-Sleep 1
+                }
+            }
+            $DownloadedSize = $DownloadedSize + (Get-Item $OutFile).Length
+            Write-Host "$($Uri): Download completed" -ForegroundColor Green
+        }
+    }
+    END {}
+}
